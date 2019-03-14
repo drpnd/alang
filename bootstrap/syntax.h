2ad64f9572c89e278485d19db18dca13a39c80ad
Hirochika Asai <panda@jar.jp>
2019-03-14 20:45:33 +0900

    Fix a typo.

--- bootstrap/syntax.h
+++ bootstrap/syntax.h
@@ -232,7 +232,7 @@ extern "C" {
     arg_t * arg_new(decl_t *);
     arg_t * arg_prepend(arg_t *, arg_t *);
     func_t * func_new(char *, arg_t *, arg_t *, stmt_list_t *);
-    stmt_t * stmt_new(expr_t *);
+    stmt_t * stmt_new_expr(expr_t *);
 
 #ifdef __cplusplus
 }
