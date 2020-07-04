0843b6a740bb39f4a29d969bedbbc31cadc4d4f4
Hirochika Asai <panda@jar.jp>
2020-07-05 01:44:49 +0900

    Adding x86-64 assembler.

--- bootstrap/arch/x86-64/x86-64.c
+++ bootstrap/arch/x86-64/x86-64.c
@@ -21,6 +21,415 @@
  * SOFTWARE.
  */
 
+#include <stdint.h>
+
+/*
+ * x86-64 registers
+ */
+typedef enum {
+    REG_UNKNOWN = -1,
+    /* IP */
+    REG_EIP = 0,
+    REG_RIP,
+    /* AX */
+    REG_AL,
+    REG_AH,
+    REG_AX,
+    REG_EAX,
+    REG_RAX,
+    /* CX */
+    REG_CL,
+    REG_CH,
+    REG_CX,
+    REG_ECX,
+    REG_RCX,
+    /* DX */
+    REG_DL,
+    REG_DH,
+    REG_DX,
+    REG_EDX,
+    REG_RDX,
+    /* BX */
+    REG_BL,
+    REG_BH,
+    REG_BX,
+    REG_EBX,
+    REG_RBX,
+    /* SP */
+    REG_SPL,
+    REG_SP,
+    REG_ESP,
+    REG_RSP,
+    /* BP */
+    REG_BPL,
+    REG_BP,
+    REG_EBP,
+    REG_RBP,
+    /* SI */
+    REG_SIL,
+    REG_SI,
+    REG_ESI,
+    REG_RSI,
+    /* DI */
+    REG_DIL,
+    REG_DI,
+    REG_EDI,
+    REG_RDI,
+    /* R8 */
+    REG_R8L,
+    REG_R8W,
+    REG_R8D,
+    REG_R8,
+    /* R9 */
+    REG_R9L,
+    REG_R9W,
+    REG_R9D,
+    REG_R9,
+    /* R10 */
+    REG_R10L,
+    REG_R10W,
+    REG_R10D,
+    REG_R10,
+    /* R11 */
+    REG_R11L,
+    REG_R11W,
+    REG_R11D,
+    REG_R11,
+    /* R12 */
+    REG_R12L,
+    REG_R12W,
+    REG_R12D,
+    REG_R12,
+    /* R13 */
+    REG_R13L,
+    REG_R13W,
+    REG_R13D,
+    REG_R13,
+    /* R14 */
+    REG_R14L,
+    REG_R14W,
+    REG_R14D,
+    REG_R14,
+    /* R15 */
+    REG_R15L,
+    REG_R15W,
+    REG_R15D,
+    REG_R15,
+    /* Segment registers */
+    REG_CS,
+    REG_DS,
+    REG_ES,
+    REG_FS,
+    REG_GS,
+    /* Flags */
+    REG_FLAGS,
+    REG_EFLAGS,
+    REG_RFLAGS,
+    /* MM */
+    REG_MM0,
+    REG_MM1,
+    REG_MM2,
+    REG_MM3,
+    REG_MM4,
+    REG_MM5,
+    REG_MM6,
+    REG_MM7,
+    /* XMM */
+    REG_XMM0,
+    REG_XMM1,
+    REG_XMM2,
+    REG_XMM3,
+    REG_XMM4,
+    REG_XMM5,
+    REG_XMM6,
+    REG_XMM7,
+} x86_64_reg_t;
+
+typedef enum {
+    REX_NE,
+    REX_NONE,
+    REX_TRUE,
+    REX_FALSE,
+} x86_64_rex_t;
+
+/*     return (1<<6) | (w<<3) | (r<<2) | (x<<1) | b; */
+#define REX             (1<<6)
+#define REX_W           (1<<3)
+#define REX_R           (1<<2)
+#define REX_X           (1<<1)
+#define REX_B           (1)
+
+typedef struct {
+    int l;
+    uint8_t a[32];
+} instr;
+
+/*
+ * Encode ModR/M
+ */
+static int
+_encode_modrm(uint8_t *modrm, int reg, int mod, int rm)
+{
+    if ( reg < 0 || reg > 7 || mod < 0 || mod > 3 || rm < 0 || rm > 7 ) {
+        return -1;
+    }
+    *modrm = (mod << 6) | (reg << 3) | rm;
+
+    return 0;
+}
+
+/*
+ * Encode SIB
+ */
+static int
+_encode_sib(uint8_t *sib, int base, int idx, int ss)
+{
+    if ( base < 0 || base > 7 || idx < 0 || idx > 7 || ss < 0 || ss > 3 ) {
+        return -1;
+    }
+    *sib = (ss << 6) | (idx << 3) | base;
+
+    return 0;
+}
+
+/*
+ * Convert register to the code
+ */
+static int
+_reg2code(uint8_t *code, x86_64_rex_t *rex, int *size,
+          x86_64_reg_t reg)
+{
+    /* Code & REX prefix */
+    switch ( reg ) {
+    case REG_AL:
+    case REG_AX:
+    case REG_EAX:
+    case REG_RAX:
+        *code = 0;
+        *rex = REX_NONE;
+        break;
+    case REG_CL:
+    case REG_CX:
+    case REG_ECX:
+    case REG_RCX:
+        *code = 1;
+        *rex = REX_NONE;
+        break;
+    case REG_DL:
+    case REG_DX:
+    case REG_EDX:
+    case REG_RDX:
+        *code = 2;
+        *rex = REX_NONE;
+        break;
+    case REG_BL:
+    case REG_BX:
+    case REG_EBX:
+    case REG_RBX:
+        *code = 3;
+        *rex = REX_NONE;
+        break;
+    case REG_AH:
+        *code = 4;
+        *rex = REX_NE;
+        break;
+    case REG_SP:
+    case REG_ESP:
+    case REG_RSP:
+        *code = 4;
+        *rex = REX_NONE;
+        break;
+    case REG_SPL:
+        *code = 4;
+        *rex = REX_FALSE;
+        break;
+    case REG_CH:
+        *code = 5;
+        *rex = REX_NE;
+        break;
+    case REG_BP:
+    case REG_EBP:
+    case REG_RBP:
+        *code = 5;
+        *rex = REX_NONE;
+        break;
+    case REG_BPL:
+        *code = 5;
+        *rex = REX_FALSE;
+        break;
+    case REG_DH:
+        *code = 6;
+        *rex = REX_NE;
+        break;
+    case REG_SI:
+    case REG_ESI:
+    case REG_RSI:
+        *code = 6;
+        *rex = REX_NONE;
+        break;
+    case REG_SIL:
+        *code = 6;
+        *rex = REX_FALSE;
+        break;
+    case REG_BH:
+        *code = 7;
+        *rex = REX_NE;
+        break;
+    case REG_DI:
+    case REG_EDI:
+    case REG_RDI:
+        *code = 7;
+        *rex = REX_NONE;
+        break;
+    case REG_DIL:
+        *code = 7;
+        *rex = REX_FALSE;
+        break;
+    case REG_R8L:
+    case REG_R8W:
+    case REG_R8D:
+    case REG_R8:
+        *code = 0;
+        *rex = REX_TRUE;
+        break;
+    case REG_R9L:
+    case REG_R9W:
+    case REG_R9D:
+    case REG_R9:
+        *code = 1;
+        *rex = REX_TRUE;
+        break;
+    case REG_R10L:
+    case REG_R10W:
+    case REG_R10D:
+    case REG_R10:
+        *code = 2;
+        *rex = REX_TRUE;
+        break;
+    case REG_R11L:
+    case REG_R11W:
+    case REG_R11D:
+    case REG_R11:
+        *code = 3;
+        *rex = REX_TRUE;
+        break;
+    case REG_R12L:
+    case REG_R12W:
+    case REG_R12D:
+    case REG_R12:
+        *code = 4;
+        *rex = REX_TRUE;
+        break;
+    case REG_R13L:
+    case REG_R13W:
+    case REG_R13D:
+    case REG_R13:
+        *code = 5;
+        *rex = REX_TRUE;
+        break;
+    case REG_R14L:
+    case REG_R14W:
+    case REG_R14D:
+    case REG_R14:
+        *code = 6;
+        *rex = REX_TRUE;
+        break;
+    case REG_R15L:
+    case REG_R15W:
+    case REG_R15D:
+    case REG_R15:
+        *code = 7;
+        *rex = REX_TRUE;
+        break;
+    default:
+        return -1;
+    }
+
+    /* Size */
+    switch ( reg ) {
+    case REG_AL:
+    case REG_AH:
+    case REG_CL:
+    case REG_CH:
+    case REG_DL:
+    case REG_DH:
+    case REG_BL:
+    case REG_BH:
+    case REG_SPL:
+    case REG_BPL:
+    case REG_SIL:
+    case REG_DIL:
+    case REG_R8L:
+    case REG_R9L:
+    case REG_R10L:
+    case REG_R11L:
+    case REG_R12L:
+    case REG_R13L:
+    case REG_R14L:
+    case REG_R15L:
+        *size = 1;
+        break;
+    case REG_AX:
+    case REG_CX:
+    case REG_DX:
+    case REG_BX:
+    case REG_SP:
+    case REG_BP:
+    case REG_SI:
+    case REG_DI:
+    case REG_R8W:
+    case REG_R9W:
+    case REG_R10W:
+    case REG_R11W:
+    case REG_R12W:
+    case REG_R13W:
+    case REG_R14W:
+    case REG_R15W:
+        *size = 2;
+        break;
+    case REG_EAX:
+    case REG_ECX:
+    case REG_EDX:
+    case REG_EBX:
+    case REG_ESP:
+    case REG_EBP:
+    case REG_ESI:
+    case REG_EDI:
+    case REG_R8D:
+    case REG_R9D:
+    case REG_R10D:
+    case REG_R11D:
+    case REG_R12D:
+    case REG_R13D:
+    case REG_R14D:
+    case REG_R15D:
+        *size = 4;
+        break;
+    case REG_RAX:
+    case REG_RCX:
+    case REG_RDX:
+    case REG_RBX:
+    case REG_RSP:
+    case REG_RBP:
+    case REG_RSI:
+    case REG_RDI:
+    case REG_R8:
+    case REG_R9:
+    case REG_R10:
+    case REG_R11:
+    case REG_R12:
+    case REG_R13:
+    case REG_R14:
+    case REG_R15:
+        *size = 8;;
+        break;
+    default:
+        return -1;
+    }
+
+    return 0;
+}
+
 /*
  * Local variables:
  * tab-width: 4
