9135796d6bd53d8239eb3116c25ce044ed6f6c4b
Hirochika Asai <panda@jar.jp>
2022-06-01 15:23:36 +0900

    Add temporary prototype declarations.

--- bootstrap/arch/x86-64/x86-64.c
+++ bootstrap/arch/x86-64/x86-64.c
@@ -1,5 +1,5 @@
 /*_
- * Copyright (c) 2020-2021 Hirochika Asai <asai@jar.jp>
+ * Copyright (c) 2020-2022 Hirochika Asai <asai@jar.jp>
  * All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -56,6 +56,9 @@
 #define OVERRIDE_OPERAND_SIZE   0x66
 /* Group 4 */
 #define OVERRIDE_ADDR_SIZE  0x67
+
+void * x86_64_initialize(void *);
+int x86_64_load_instr(void *);
 
 /*
  * Encode ModR/M
