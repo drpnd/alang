5201489cef761e1718a0fc09ca9dc0e43223d7ae
Hirochika Asai <panda@jar.jp>
2026-09-09 11:08:57 +0900

    Rewrite ir.h/ir.c with DFIR (Data Flow IR) specification
    - 44 opcodes organized by category: SSA values (const, phi), memory,
      arithmetic, comparison, logical/bitwise, cast, struct/enum, control
      flow (br, br_cond, switch, ret), call, and coro-specific (recv, send,
      yield, await, suspend)
    - SSA registers with ir_reg_type_t including low-precision floats
      (f16, fp8, fp4)
    - Graph layer (ir_graph_t, ir_graph_node_t, ir_graph_edge_t) for
      DFIR Layer 3 topology declarations
    - Structure builders: ir_object_add_func, ir_func_add_block,
      ir_block_add_instr, ir_graph_add_node, ir_graph_add_edge
    - Opcode metadata: ir_num_results, ir_num_operands,
      ir_opcode_is_terminator, ir_opcode_is_coro_only
    - Fixed ir_reg_release memory leak (now frees reg->id)
    - Fixed ir_object_add_graph self-comparison bug (proper capacity tracking)

--- bootstrap/ir.c
+++ bootstrap/ir.c
@@ -1,214 +1,224 @@
 /*_
  * Copyright (c) 2022-2024,2026 Hirochika Asai <asai@jar.jp>
  * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * MIT License
  */
 
 #include "ir.h"
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * ir_object_new -- allocate a new object
- */
-ir_object_t *
-ir_object_new(void)
-{
-    ir_object_t *obj;
-
-    obj = malloc(sizeof(ir_object_t));
-    if ( obj == NULL ) {
-        return NULL;
-    }
-    memset(obj, 0, sizeof(ir_object_t));
-
+ir_object_t *ir_object_new(void) {
+    ir_object_t *obj = malloc(sizeof(ir_object_t));
+    if (obj) memset(obj, 0, sizeof(ir_object_t));
     return obj;
 }
 
-/*
- * ir_func_new -- allocate a new function
- */
-ir_func_t *
-ir_func_new(void)
-{
-    ir_func_t *func;
-
-    func = malloc(sizeof(ir_func_t));
-    if ( func == NULL ) {
-        return NULL;
-    }
-    memset(func, 0, sizeof(ir_func_t));
-
-    return func;
+ir_func_t *ir_func_new(void) {
+    ir_func_t *f = malloc(sizeof(ir_func_t));
+    if (f) memset(f, 0, sizeof(ir_func_t));
+    return f;
 }
 
-/*
- * ir_instr_new -- allocate a new instruction
- */
-ir_instr_t *
-ir_instr_new(void)
-{
-    ir_instr_t *i;
+ir_graph_t *ir_graph_new(const char *name) {
+    ir_graph_t *g = malloc(sizeof(ir_graph_t));
+    if (g) { memset(g, 0, sizeof(ir_graph_t)); if (name) g->name = strdup(name); }
+    return g;
+}
 
-    i = malloc(sizeof(ir_instr_t));
-    if ( i == NULL ) {
-        return NULL;
+ir_block_t *ir_block_new(const char *label_name) {
+    ir_block_t *b = malloc(sizeof(ir_block_t));
+    if (b) {
+        memset(b, 0, sizeof(ir_block_t));
+        if (label_name) {
+            b->label = malloc(sizeof(ir_label_t));
+            if (b->label) {
+                b->label->name = strdup(label_name);
+                b->label->block = b;
+            }
+        }
     }
-    memset(i, 0, sizeof(ir_instr_t));
+    return b;
+}
 
+ir_instr_t *ir_instr_new(void) {
+    ir_instr_t *i = malloc(sizeof(ir_instr_t));
+    if (i) memset(i, 0, sizeof(ir_instr_t));
     return i;
 }
 
-/*
- * ir_instr_delete -- delete an instruction
- */
-void
-ir_instr_delete(ir_instr_t *i)
-{
-    free(i);
+void ir_instr_delete(ir_instr_t *i) { free(i); }
+
+ir_instr_ent_t *ir_instr_ent_new(void) {
+    ir_instr_ent_t *e = malloc(sizeof(ir_instr_ent_t));
+    if (e) memset(e, 0, sizeof(ir_instr_ent_t));
+    return e;
 }
 
-/*
- * ir_reg_init -- initialize a new register
- */
-ir_reg_t *
-ir_reg_init(ir_reg_t *reg)
-{
+void ir_instr_ent_delete(ir_instr_ent_t *e) { free(e); }
+
+ir_reg_t *ir_reg_init(ir_reg_t *reg, ir_reg_type_t type, const char *id) {
+    if (!reg) return NULL;
+    reg->type = type;
+    reg->id = id ? strdup(id) : NULL;
     return reg;
 }
 
-/*
- * ir_reg_release -- destruct a register
- */
-void
-ir_reg_release(ir_reg_t *reg)
-{
+void ir_reg_release(ir_reg_t *reg) {
+    if (reg && reg->id) { free(reg->id); reg->id = NULL; }
 }
 
-/*
- * ir_imm_init -- initialize a new immediate value
- */
-ir_imm_t *
-ir_imm_init(ir_imm_t *imm, ir_imm_type_t type)
-{
+ir_imm_t *ir_imm_init(ir_imm_t *imm, ir_imm_type_t type) {
+    if (!imm) return NULL;
+    memset(imm, 0, sizeof(ir_imm_t));
     imm->type = type;
     return imm;
 }
 
-/*
- * ir_imm_release -- destruct an immediate value
- */
-void
-ir_imm_release(ir_imm_t *imm)
-{
-    switch ( imm->type ) {
-    default:
-        /* Do nothing */
-        break;
-    }
+void ir_imm_release(ir_imm_t *imm) {
+    if (!imm) return;
+    if (imm->type == IR_IMM_STR && imm->u.str) { free(imm->u.str); imm->u.str = NULL; }
 }
 
-/*
- * ir_operand_new -- allocate a new operand
- */
-ir_operand_t *
-ir_operand_new(void)
-{
-    ir_operand_t *o;
-
-    o = malloc(sizeof(ir_operand_t));
-    if ( o == NULL ) {
-        return NULL;
-    }
-    memset(o, 0, sizeof(ir_operand_t));
-
+ir_operand_t *ir_operand_new(void) {
+    ir_operand_t *o = malloc(sizeof(ir_operand_t));
+    if (o) memset(o, 0, sizeof(ir_operand_t));
     return o;
 }
 
-/*
- * ir_operand_delete -- delete an operand
- */
-void
-ir_operand_delete(ir_operand_t *o)
-{
-    free(o);
+void ir_operand_delete(ir_operand_t *o) { free(o); }
+
+int ir_object_add_func(ir_object_t *obj, ir_func_t *func) {
+    if (!obj || !func) return -1;
+    if (!obj->funcs) { obj->funcs = func; }
+    else { ir_func_t *t = obj->funcs; while (t->next) t = t->next; t->next = func; }
+    obj->nfuncs++;
+    return 0;
 }
 
-/*
- * ir_num_results -- return the number of results of the specified opcode
- */
-int
-ir_num_results(ir_opcode_t opcode)
-{
-    int cnt;
-
-    switch ( opcode ) {
-    case IR_OPCODE_STORE:
-        cnt = 0;
-        break;
-    case IR_OPCODE_ALLOCA:
-    case IR_OPCODE_LOAD:
-        cnt = 1;
-        break;
-    default:
-        cnt = -1;
+int ir_object_add_graph(ir_object_t *obj, ir_graph_t *graph) {
+    if (!obj || !graph) return -1;
+    if (obj->ngraphs >= obj->graphs_cap) {
+        size_t nc = obj->graphs_cap ? obj->graphs_cap * 2 : 8;
+        ir_graph_t *na = realloc(obj->graphs, nc * sizeof(ir_graph_t));
+        if (!na) return -1;
+        obj->graphs = na; obj->graphs_cap = nc;
     }
-
-    return cnt;
+    obj->graphs[obj->ngraphs++] = *graph;
+    return 0;
 }
 
-/*
- * ir_num_operands -- return the number of operands for the specified opcode
- */
-int
-ir_num_operands(ir_opcode_t opcode)
-{
-    int cnt;
+int ir_func_add_block(ir_func_t *func, ir_block_t *block) {
+    if (!func || !block) return -1;
+    ir_block_t *nb = realloc(func->blocks, (func->nblocks + 1) * sizeof(ir_block_t));
+    if (!nb) return -1;
+    func->blocks = nb;
+    func->blocks[func->nblocks++] = *block;
+    return 0;
+}
 
-    switch ( opcode ) {
-    case IR_OPCODE_INC:
-    case IR_OPCODE_DEC:
-        cnt = 1;
-        break;
-    case IR_OPCODE_MOV:
-        cnt = 2;
-        break;
-    case IR_OPCODE_ADD:
-    case IR_OPCODE_SUB:
-    case IR_OPCODE_MUL:
-    case IR_OPCODE_DIV:
-    case IR_OPCODE_MOD:
-        cnt = 3;
-        break;
-    default:
-        cnt = -1;
+int ir_graph_add_node(ir_graph_t *graph, const char *name, const char *func_ref,
+                      int nports, const char **port_names) {
+    if (!graph || !name || !func_ref) return -1;
+    ir_graph_node_t *nn = realloc(graph->nodes, (graph->nnodes + 1) * sizeof(ir_graph_node_t));
+    if (!nn) return -1;
+    graph->nodes = nn;
+    ir_graph_node_t *n = &graph->nodes[graph->nnodes++];
+    memset(n, 0, sizeof(*n));
+    n->name = strdup(name); n->func_ref = strdup(func_ref); n->nports = nports;
+    if (nports > 0 && port_names) {
+        n->port_names = malloc(nports * sizeof(char *));
+        if (!n->port_names) return -1;
+        for (int i = 0; i < nports; i++) n->port_names[i] = strdup(port_names[i]);
     }
-
-    return cnt;
+    return 0;
 }
 
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: sw=4 ts=4 fdm=marker
- * vim<600: sw=4 ts=4
- */
+int ir_graph_add_edge(ir_graph_t *graph, const char *src_node, const char *src_port,
+                      const char *dst_node, const char *dst_port,
+                      ir_reg_type_t elem_type, int bufsize) {
+    if (!graph || !src_node || !src_port || !dst_node || !dst_port) return -1;
+    ir_graph_edge_t *ne = realloc(graph->edges, (graph->nedges + 1) * sizeof(ir_graph_edge_t));
+    if (!ne) return -1;
+    graph->edges = ne;
+    ir_graph_edge_t *e = &graph->edges[graph->nedges++];
+    memset(e, 0, sizeof(*e));
+    e->src_node = strdup(src_node); e->src_port = strdup(src_port);
+    e->dst_node = strdup(dst_node); e->dst_port = strdup(dst_port);
+    e->elem_type = elem_type; e->bufsize = bufsize;
+    return 0;
+}
+
+int ir_block_add_instr(ir_block_t *block, ir_instr_t *instr) {
+    if (!block || !instr) return -1;
+    ir_instr_ent_t *ent = ir_instr_ent_new();
+    if (!ent) return -1;
+    ent->inst = *instr; ent->next = NULL;
+    if (block->instrs) block->last->next = ent;
+    else block->instrs = ent;
+    block->last = ent; block->ninstr++;
+    return 0;
+}
+
+int ir_num_results(ir_opcode_t o) {
+    switch (o) {
+    case IR_OPCODE_STORE: case IR_OPCODE_MEMCPY: case IR_OPCODE_SET_FIELD:
+    case IR_OPCODE_BR: case IR_OPCODE_BR_COND: case IR_OPCODE_SWITCH:
+    case IR_OPCODE_RET: case IR_OPCODE_YIELD: case IR_OPCODE_SUSPEND: return 0;
+    case IR_OPCODE_CONST: case IR_OPCODE_PHI: case IR_OPCODE_ALLOCA:
+    case IR_OPCODE_LOAD: case IR_OPCODE_MOV: case IR_OPCODE_ADD:
+    case IR_OPCODE_SUB: case IR_OPCODE_MUL: case IR_OPCODE_DIV:
+    case IR_OPCODE_UDIV: case IR_OPCODE_MOD: case IR_OPCODE_UREM:
+    case IR_OPCODE_NEG: case IR_OPCODE_CMP_EQ: case IR_OPCODE_CMP_NE:
+    case IR_OPCODE_CMP_LT: case IR_OPCODE_CMP_LE: case IR_OPCODE_CMP_GT:
+    case IR_OPCODE_CMP_GE: case IR_OPCODE_NOT: case IR_OPCODE_AND:
+    case IR_OPCODE_OR: case IR_OPCODE_XOR: case IR_OPCODE_SHL:
+    case IR_OPCODE_SHR: case IR_OPCODE_CAST: case IR_OPCODE_MAKE_STRUCT:
+    case IR_OPCODE_GET_FIELD: case IR_OPCODE_GET_ELEM:
+    case IR_OPCODE_MAKE_ENUM: case IR_OPCODE_EXTRACT_VARIANT:
+    case IR_OPCODE_CHECK_VARIANT: case IR_OPCODE_CALL:
+    case IR_OPCODE_RECV: case IR_OPCODE_SEND: case IR_OPCODE_AWAIT: return 1;
+    default: return -1;
+    }
+}
+
+int ir_num_operands(ir_opcode_t o) {
+    switch (o) {
+    case IR_OPCODE_ALLOCA: case IR_OPCODE_SUSPEND: return 0;
+    case IR_OPCODE_CONST: case IR_OPCODE_LOAD: case IR_OPCODE_MOV:
+    case IR_OPCODE_NEG: case IR_OPCODE_NOT: case IR_OPCODE_CAST:
+    case IR_OPCODE_GET_FIELD: case IR_OPCODE_MAKE_ENUM:
+    case IR_OPCODE_EXTRACT_VARIANT: case IR_OPCODE_CHECK_VARIANT:
+    case IR_OPCODE_BR: case IR_OPCODE_RET:
+    case IR_OPCODE_RECV: case IR_OPCODE_AWAIT: return 1;
+    case IR_OPCODE_STORE: case IR_OPCODE_ADD: case IR_OPCODE_SUB:
+    case IR_OPCODE_MUL: case IR_OPCODE_DIV: case IR_OPCODE_UDIV:
+    case IR_OPCODE_MOD: case IR_OPCODE_UREM: case IR_OPCODE_CMP_EQ:
+    case IR_OPCODE_CMP_NE: case IR_OPCODE_CMP_LT: case IR_OPCODE_CMP_LE:
+    case IR_OPCODE_CMP_GT: case IR_OPCODE_CMP_GE: case IR_OPCODE_AND:
+    case IR_OPCODE_OR: case IR_OPCODE_XOR: case IR_OPCODE_SHL:
+    case IR_OPCODE_SHR: case IR_OPCODE_GET_ELEM:
+    case IR_OPCODE_SEND: case IR_OPCODE_YIELD: return 2;
+    case IR_OPCODE_MEMCPY: case IR_OPCODE_BR_COND:
+    case IR_OPCODE_SET_FIELD: return 3;
+    case IR_OPCODE_PHI: case IR_OPCODE_SWITCH:
+    case IR_OPCODE_CALL: case IR_OPCODE_MAKE_STRUCT: return -1;
+    default: return -1;
+    }
+}
+
+int ir_opcode_is_terminator(ir_opcode_t o) {
+    switch (o) {
+    case IR_OPCODE_BR: case IR_OPCODE_BR_COND:
+    case IR_OPCODE_SWITCH: case IR_OPCODE_RET: case IR_OPCODE_SUSPEND: return 1;
+    default: return 0;
+    }
+}
+
+int ir_opcode_is_coro_only(ir_opcode_t o) {
+    switch (o) {
+    case IR_OPCODE_RECV: case IR_OPCODE_SEND:
+    case IR_OPCODE_YIELD: case IR_OPCODE_AWAIT: case IR_OPCODE_SUSPEND: return 1;
+    default: return 0;
+    }
+}
--- bootstrap/ir.h
+++ bootstrap/ir.h
@@ -25,258 +25,112 @@
 #define _IR_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <unistd.h>
 
-/*
- * Opcode
- */
 typedef enum {
-    /* Memory operations */
-    IR_OPCODE_ALLOCA,   /* %reg = alloca <type> */
-    IR_OPCODE_LOAD,     /* %reg = load <type>* <ptr> */
-    IR_OPCODE_STORE,    /* store <type> <value> <type>* <ptr> */
-    IR_OPCODE_MOV,      /* src,dst */
-    /* Arithmetic operations */
-    IR_OPCODE_ADD,      /* %reg = %op1,%op2 */
-    IR_OPCODE_SUB,      /* %reg = op1,op2 */
-    IR_OPCODE_MUL,      /* %reg = op1,op2 */
-    IR_OPCODE_DIV,      /* %q,%r = op1,op2 */
-    IR_OPCODE_MOD,      /* %r,%q = op1,op2 */
-    IR_OPCODE_INC,      /* op */
-    IR_OPCODE_DEC,      /* op */
-    /* Logical operations */
-    IR_OPCODE_NOT,      /* op,dst */
-    IR_OPCODE_COMP,     /* op,dst */
-    IR_OPCODE_LAND,     /* op1,op2,dst */
-    IR_OPCODE_LOR,      /* op1,op2,dst */
-    /* Bit-wise operations */
-    IR_OPCODE_AND,      /* op1,op2,dst */
-    IR_OPCODE_OR,       /* op1,op2,dst */
-    IR_OPCODE_XOR,      /* op1,op2,dst */
-    IR_OPCODE_LSHIFT,   /* op1,op2,dst */
-    IR_OPCODE_RSHIFT,   /* op1,op2,dst */
-    /* Controls */
-    IR_OPCODE_CMP_EQ,   /* op1,op2,dst */
-    IR_OPCODE_CMP_NEQ,  /* op1,op2,dst */
-    IR_OPCODE_CMP_GT,   /* op1,op2,dst */
-    IR_OPCODE_CMP_LT,   /* op1,op2,dst */
-    IR_OPCODE_CMP_GEQ,  /* op1,op2,dst */
-    IR_OPCODE_CMP_LEQ,  /* op1,op2,dst */
-    IR_OPCODE_RET,      /* no operands */
-    IR_OPCODE_YIELD,    /* no operands */
+    IR_OPCODE_CONST, IR_OPCODE_PHI,
+    IR_OPCODE_ALLOCA, IR_OPCODE_LOAD, IR_OPCODE_STORE, IR_OPCODE_MOV, IR_OPCODE_MEMCPY,
+    IR_OPCODE_ADD, IR_OPCODE_SUB, IR_OPCODE_MUL, IR_OPCODE_DIV, IR_OPCODE_UDIV,
+    IR_OPCODE_MOD, IR_OPCODE_UREM, IR_OPCODE_NEG,
+    IR_OPCODE_CMP_EQ, IR_OPCODE_CMP_NE, IR_OPCODE_CMP_LT, IR_OPCODE_CMP_LE,
+    IR_OPCODE_CMP_GT, IR_OPCODE_CMP_GE,
+    IR_OPCODE_NOT, IR_OPCODE_AND, IR_OPCODE_OR, IR_OPCODE_XOR, IR_OPCODE_SHL, IR_OPCODE_SHR,
+    IR_OPCODE_CAST,
+    IR_OPCODE_MAKE_STRUCT, IR_OPCODE_GET_FIELD, IR_OPCODE_SET_FIELD, IR_OPCODE_GET_ELEM,
+    IR_OPCODE_MAKE_ENUM, IR_OPCODE_EXTRACT_VARIANT, IR_OPCODE_CHECK_VARIANT,
+    IR_OPCODE_BR, IR_OPCODE_BR_COND, IR_OPCODE_SWITCH, IR_OPCODE_RET,
+    IR_OPCODE_CALL,
+    IR_OPCODE_RECV, IR_OPCODE_SEND, IR_OPCODE_YIELD, IR_OPCODE_AWAIT, IR_OPCODE_SUSPEND,
 } ir_opcode_t;
 
-/*
- * Operand type
- */
 typedef enum {
-    OPERAND_VAL,
-    OPERAND_REF,
-    OPERAND_I8,
-    OPERAND_I16,
-    OPERAND_I32,
-    OPERAND_I64,
-    OPERAND_FP32,
-    OPERAND_FP64,
-} operand_type_t;
-
-/*
- * Operand type
- */
-typedef enum {
-    OPERAND_TYPE_REG,
-    OPERAND_TYPE_REF,
-    OPERAND_TYPE_IMM,
-} ir_operand_type_t;
-
-/*
- * Operand size
- */
-typedef enum {
-    OPERAND_SIZE_AUTO,
-    OPERAND_SIZE_I8,
-    OPERAND_SIZE_I16,
-    OPERAND_SIZE_I32,
-    OPERAND_SIZE_I64,
-    OPERAND_SIZE_FP32,
-    OPERAND_SIZE_FP64,
-} ir_operand_size_t;
-
-/*
- * Immediate value type
- */
-typedef enum {
-    IR_IMM_I8,
-    IR_IMM_S8,
-    IR_IMM_I16,
-    IR_IMM_S16,
-    IR_IMM_I32,
-    IR_IMM_S32,
-    IR_IMM_I64,
-    IR_IMM_S64,
+    IR_IMM_I8, IR_IMM_S8, IR_IMM_I16, IR_IMM_S16,
+    IR_IMM_I32, IR_IMM_S32, IR_IMM_I64, IR_IMM_S64,
+    IR_IMM_F16, IR_IMM_F32, IR_IMM_F64, IR_IMM_FP8, IR_IMM_FP4,
+    IR_IMM_BOOL, IR_IMM_STR,
 } ir_imm_type_t;
 
-/*
- * Data type
- */
-typedef enum {
-    IR_DATA_DATA,
-    IR_DATA_BSS,
-    IR_DATA_RODATA,
-} ir_data_type_t;
+typedef enum { IR_DATA_DATA, IR_DATA_BSS, IR_DATA_RODATA, } ir_data_type_t;
 
-/*
- * Register type
- */
 typedef enum {
-    IR_REG_UNDEF = -1,
-    IR_REG_PTR,
-    IR_REG_I8,
-    IR_REG_I16,
-    IR_REG_I32,
-    IR_REG_I64,
-    IR_REG_FP32,
-    IR_REG_FP64,
-    IR_REG_BOOL,
+    IR_REG_NONE = 0, IR_REG_PTR,
+    IR_REG_I8, IR_REG_I16, IR_REG_I32, IR_REG_I64,
+    IR_REG_F16, IR_REG_F32, IR_REG_F64, IR_REG_FP8, IR_REG_FP4,
+    IR_REG_BOOL, IR_REG_STR, IR_REG_VOID,
 } ir_reg_type_t;
 
-/*
- * Register
- */
-typedef struct {
-    ir_reg_type_t type;
-    int assigned;
-    char *id;
-} ir_reg_t;
+typedef struct { ir_reg_type_t type; char *id; } ir_reg_t;
 
-/*
- * Immediate value
- */
 typedef struct {
     ir_imm_type_t type;
     union {
-        /* Integers */
-        uint8_t u8;
-        int8_t s8;
-        uint16_t u16;
-        int16_t s16;
-        uint32_t u32;
-        int32_t s32;
-        uint64_t u64;
-        int64_t s64;
+        uint8_t u8; int8_t s8; uint16_t u16; int16_t s16;
+        uint32_t u32; int32_t s32; uint64_t u64; int64_t s64;
+        _Float16 f16; float f32; double f64;
+        uint8_t fp8; uint8_t fp4; bool bval; char *str;
     } u;
 } ir_imm_t;
 
-/*
- * Reference (pointer)
- */
-typedef struct {
-    ir_reg_t base;
-    ir_reg_t index;
-    int scale;
-    int64_t disp;
-} ir_ref_t;
+typedef enum { IR_OPERAND_REG, IR_OPERAND_IMM, IR_OPERAND_REF, IR_OPERAND_LABEL, } ir_operand_type_t;
 
-/*
- * Operand
- */
+typedef struct { ir_reg_t base; ir_reg_t index; int scale; int64_t disp; } ir_ref_t;
+
 typedef struct {
     ir_operand_type_t type;
-    union {
-        ir_reg_t reg;
-        ir_imm_t imm;
-        ir_ref_t ref;
-    } u;
+    union { ir_reg_t reg; ir_imm_t imm; ir_ref_t ref; char *label; } u;
 } ir_operand_t;
 
-/*
- * Result
- */
-typedef struct {
-    int n;
-    ir_reg_t reg[2];
-} ir_result_t;
+typedef struct { int n; ir_reg_t reg[2]; } ir_result_t;
 
-/*
- * Instruction
- */
+#define IR_MAX_OPERANDS 8
+
 typedef struct {
-    ir_opcode_t opcode;
-    ir_result_t result;
-    ir_operand_t operands[4];
+    ir_opcode_t opcode; ir_result_t result; int noperands;
+    ir_operand_t operands[IR_MAX_OPERANDS];
 } ir_instr_t;
 
 typedef struct _instr_ent ir_instr_ent_t;
 typedef struct _block ir_block_t;
-/*
- * Instruction entry
- */
-struct _instr_ent {
-    ir_instr_t inst;
-    ir_instr_ent_t *next;
-};
-
-/*
- * Label
- */
-typedef struct {
-    char *label;
-    ir_block_t *block;
-} ir_label_t;
-
-/*
- * Block
- */
-struct _block {
-    ir_label_t *label;
-    size_t ninstr;
-    /* Pointer to the insstruction */
-    ir_instr_ent_t *instrs;
-};
-
-/*
- * Type of function
- */
-typedef enum {
-    IR_FUNC_FUNC,
-    IR_FUNC_COROUTINE,
-} ir_func_type_t;
-
-/*
- * Function / Coroutine
- */
 typedef struct _func ir_func_t;
-struct _func {
-    char *name;
-    ir_func_type_t type;
-    size_t nblocks;
-    ir_block_t *blocks;
-    ir_func_t *next;
+
+struct _instr_ent { ir_instr_t inst; ir_instr_ent_t *next; };
+
+typedef struct { char *name; ir_block_t *block; } ir_label_t;
+
+struct _block {
+    ir_label_t *label; size_t ninstr;
+    ir_instr_ent_t *instrs; ir_instr_ent_t *last;
 };
 
-/*
- * Data entry
- */
-typedef struct {
-    size_t len;
-    uint8_t *d;
-} ir_data_entry_t;
+typedef enum { IR_FUNC_FUNC, IR_FUNC_COROUTINE, } ir_func_type_t;
 
-/*
- * Data table (global)
- */
-typedef struct {
-    size_t n;
-    size_t used;
-    ir_data_entry_t *entries;
-} ir_data_table_t;
+struct _func {
+    char *name; ir_func_type_t type; size_t nblocks;
+    ir_block_t *blocks; ir_func_t *next;
+};
 
-/*
- * IR object
- */
+typedef struct { ir_data_type_t type; size_t len; uint8_t *d; } ir_data_entry_t;
+typedef struct { size_t n; size_t used; ir_data_entry_t *entries; } ir_data_table_t;
+
 typedef struct {
-    size_t nfuncs;
-    ir_func_t *funcs;
+    char *name; char *func_ref; int nports; char **port_names;
+} ir_graph_node_t;
+
+typedef struct {
+    char *src_node; char *src_port; char *dst_node; char *dst_port;
+    ir_reg_type_t elem_type; int bufsize;
+} ir_graph_edge_t;
+
+typedef struct {
+    char *name; size_t nnodes; ir_graph_node_t *nodes;
+    size_t nedges; ir_graph_edge_t *edges;
+} ir_graph_t;
+
+typedef struct {
+    size_t nfuncs; ir_func_t *funcs;
+    size_t ngraphs; size_t graphs_cap; ir_graph_t *graphs;
     ir_data_table_t data;
 } ir_object_t;
 
@@ -284,47 +138,41 @@ typedef struct {
 extern "C" {
 #endif
 
-/* ir.c */
-ir_object_t *
-ir_object_new(void);
-ir_func_t *
-ir_func_new(void);
-ir_instr_t *
-ir_instr_new(void);
-void
-ir_instr_delete(ir_instr_t *);
-ir_reg_t *
-ir_reg_init(ir_reg_t *);
-void
-ir_reg_release(ir_reg_t *);
-ir_imm_t *
-ir_imm_init(ir_imm_t *, ir_imm_type_t type);
-void
-ir_imm_release(ir_imm_t *);
-ir_operand_t *
-ir_operand_new(void);
-void
-ir_operand_delete(ir_operand_t *);
-int
-ir_num_results(ir_opcode_t);
-int
-ir_num_operands(ir_opcode_t);
+ir_object_t *ir_object_new(void);
+ir_func_t *ir_func_new(void);
+ir_graph_t *ir_graph_new(const char *name);
+ir_block_t *ir_block_new(const char *label_name);
+ir_instr_t *ir_instr_new(void);
+void ir_instr_delete(ir_instr_t *);
+ir_instr_ent_t *ir_instr_ent_new(void);
+void ir_instr_ent_delete(ir_instr_ent_t *);
 
-/* ir_debug.c */
-int
-ir_print_code(ir_object_t *);
+ir_reg_t *ir_reg_init(ir_reg_t *reg, ir_reg_type_t type, const char *id);
+void ir_reg_release(ir_reg_t *reg);
+ir_imm_t *ir_imm_init(ir_imm_t *imm, ir_imm_type_t type);
+void ir_imm_release(ir_imm_t *imm);
+ir_operand_t *ir_operand_new(void);
+void ir_operand_delete(ir_operand_t *);
+
+int ir_object_add_func(ir_object_t *obj, ir_func_t *func);
+int ir_object_add_graph(ir_object_t *obj, ir_graph_t *graph);
+int ir_func_add_block(ir_func_t *func, ir_block_t *block);
+int ir_graph_add_node(ir_graph_t *graph, const char *name, const char *func_ref,
+                      int nports, const char **port_names);
+int ir_graph_add_edge(ir_graph_t *graph, const char *src_node, const char *src_port,
+                      const char *dst_node, const char *dst_port,
+                      ir_reg_type_t elem_type, int bufsize);
+int ir_block_add_instr(ir_block_t *block, ir_instr_t *instr);
+
+int ir_num_results(ir_opcode_t opcode);
+int ir_num_operands(ir_opcode_t opcode);
+int ir_opcode_is_terminator(ir_opcode_t opcode);
+int ir_opcode_is_coro_only(ir_opcode_t opcode);
+
+int ir_print_code(ir_object_t *);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* _IR_H */
-
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: sw=4 ts=4 fdm=marker
- * vim<600: sw=4 ts=4
- */
